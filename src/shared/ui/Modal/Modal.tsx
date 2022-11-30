e915ee29a55e33b68d03e40e1c68e4d4c737d476
Ян Неровный <yan.nerovnyi@combridge-itc.de>
2022-11-30 22:10:25 +0300

    fix theme for modal window

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -28,7 +28,6 @@ export const Modal = ({
     const mods: Record<string, boolean> = {
         [styles.opened]: isOpen,
         [styles.isClosing]: isClosing,
-        [styles[theme]]: true,
     };
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -62,7 +61,7 @@ export const Modal = ({
 
     return (
         <Portal>
-            <div className={classNames(styles.Modal, mods, [className])}>
+            <div className={classNames(styles.Modal, mods, [className, theme])}>
                 <div className={styles.overlay} onClick={closeHandler}>
                     <div className={styles.content} onClick={onContentClick}>
                         {children}
