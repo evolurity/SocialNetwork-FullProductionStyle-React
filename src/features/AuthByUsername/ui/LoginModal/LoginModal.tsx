adc39e11eafaeac4b3207a906d8eed976300b5e9
Ян Неровный <yan.nerovnyi@combridge-itc.de>
2022-12-02 04:13:17 +0300

    fix lint and update screen

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import styles from './LoginModal.module.scss';
 
 interface LoginModalProps {
     className?: string;
@@ -13,7 +12,7 @@ export const LoginModal = ({ className, isOpen, onClose }:LoginModalProps) => (
     <Modal
         isOpen={isOpen}
         onClose={onClose}
-        className={classNames(styles.LoginModal, {}, [className])}
+        className={classNames('', {}, [className])}
         lazy
     >
         <LoginForm />
